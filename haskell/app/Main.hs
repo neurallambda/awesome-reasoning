02022f44189c6f74b39b8d834ead55346dccf36a
person <freckletonj@gmail.com>
2024-04-07 16:11:51 -0700

    checkin

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,5 +1,11 @@
 {-
 
+Generate sentences in given grammars.
+
+
+----------
+NOTES:
+
 Mealy Machine (FSM): 6-tuple (Q, Σ, Δ, δ, λ, q₀), where:
 - Q is a finite set of states
 - Σ is a finite set of input symbols (the input alphabet)
@@ -51,7 +57,8 @@ import Data.Sequence (Seq, ViewL(..), (<|), (|>))
 import qualified Data.Sequence as Seq
 import Control.Arrow
 import Data.MonadicStreamFunction
-
+import Data.Foldable (toList)
+import Data.List (intercalate)
 
 --------------------------------------------------
 -- * Util
@@ -79,18 +86,18 @@ dropr i s = fst $ splitAtR i s
 --------------------------------------------------
 -- * Finite State Machine
 
-data FSMOutput f b s = FSMOutput {
-  fsmOutput :: !(f b),
-  fsmState :: !(f s)
-  } deriving (Functor, Show)
+data FSMTransition out state = FSMTransition {
+  fsmtOut :: !out,
+  fsmNewState :: !state
+  } deriving Show
 
 -- | Construct an FSM from a transition function
-fsm :: (a -> s -> FSMOutput Identity b s) -> MSF (State s) a b
+fsm :: (a -> s -> FSMTransition b s) -> MSF (State s) a b
 fsm f = arrM (\a -> do
-                 s <- gets Identity
-                 let FSMOutput b s' = f a (runIdentity s)
-                 put (runIdentity s')
-                 pure (runIdentity b))
+                 s <- get
+                 let FSMTransition b s' = f a s
+                 put s'
+                 pure b)
 
 -- | Upgrade an MSF to also output it's state
 outputState :: MSF (State s) a b -> MSF (State s) a (b, s)
@@ -100,26 +107,57 @@ outputState msf = proc a -> do
   returnA -< (b, s)
 
 -- | Run an FSM and keep a list of outputs and states
-runFSM :: MSF (State s) a b -> [a] -> s -> FSMOutput [] b s
+runFSM :: MSF (State s) a b -> [a] -> s -> ([b], [s])
 runFSM msf input initialS = let
   out = evalState (embed (outputState msf) input) initialS
-  (bs, ss) = unzip out
-  in FSMOutput bs ss
+  in unzip out
 
--- | A helper to wrap `Identity`
-fsmi :: b -> s -> FSMOutput Identity b s
-fsmi a b = FSMOutput (Identity a) (Identity b)
+-- | Run an FSM to it's final output and state
+runFSM' :: MSF (State s) a b -> [a] -> s -> (b, s)
+runFSM' msf input initialS = let
+  out = evalState (embed (outputState msf) input) initialS
+  in (fst (last out), snd (last out))
+
+-- | Evaluate an FSM and collect its outputs (not state)
+evalFSM :: MSF (State s) a b -> [a] -> s -> [b]
+evalFSM msf input initialS = let
+  out = evalState (embed msf input) initialS
+  in out
+
+-- | Evaluate an FSM to its final output
+evalFSM' :: MSF (State s) a b -> [a] -> s -> b
+evalFSM' msf input initialS = let
+  out = evalState (embed msf input) initialS
+  in last out
+
+-- | Evaluate an FSM and collect its states (not outputs)
+execFSM :: MSF (State s) a b -> [a] -> s -> [s]
+execFSM msf input initialS = let
+  msf' = proc a -> do
+    _ <- msf -< a
+    arrM gets -< id
+  out = evalState (embed msf' input) initialS
+  in out
+
+-- | Evaluate an FSM to its final state
+execFSM' :: MSF (State s) a b -> [a] -> s -> s
+execFSM' msf input initialS = let
+  msf' = proc a -> do
+    _ <- msf -< a
+    arrM gets -< id
+  out = evalState (embed msf' input) initialS
+  in last out
+
 
 -- | Example FSM for counting a's
-countA :: Char -> Int -> FSMOutput Identity () Int
-countA 'a' count = fsmi () (count + 1)
-countA _ count   = fsmi () count
+countA :: Char -> Int -> FSMTransition () Int
+countA 'a' count = FSMTransition () (count + 1)
+countA _ count   = FSMTransition () count
 
 
 --------------------------------------------------
 -- * Push down automata
 
-data Sym = A | B | C deriving (Eq, Show)
 data Q =
   Q0
   | Q1
@@ -130,7 +168,6 @@ data Q =
   | QReject
   deriving (Eq, Show)
 
-
 data PDAOp s =
   NullOp
   | Push !s
@@ -138,143 +175,68 @@ data PDAOp s =
   deriving (Eq, Show)
 
 -- | Pushdown Automaton (PDA)
-data PDATransition f b s stack = PDATransition {
-  pdaTOutput :: !(f b),
-  pdaTState :: !(f s),
-  pdaTStack :: !(f (PDAOp stack))
-  }
-
-data PDAState state stack = PDAState {
+data PDATransition b state stack = PDATransition {
+  pdaOutput :: !b,
   pdaState :: !state,
-  pdaStack :: !(Seq stack)
+  pdaStack :: !(PDAOp stack)
   }
 
--- | Construct a PDA from a transition function
-pda :: (Monad f) => (a -> s -> Maybe stack -> PDATransition Identity b s stack) -> MSF (StateT (PDAState state stack) f) a b
+type PDAState state stack = (state, Seq stack)
+
+-- | Construct an FSM from a transition function
+pda ::
+  (a -> state -> Maybe stack -> PDATransition b state stack)
+  -> MSF (State (PDAState state stack)) a b
 pda f = arrM (\a -> do
-  PDAState{..} <- get
-  let hd = viewl pdaStack
-  let PDATransition b s' stackOp = f a (runIdentity pdaState) hd
-  case runIdentity stackOp of
-    NullOp -> pure ()
-    Push x -> modify (x <|)
-    Pop    -> modify $ Seq.drop 1
-  put (runIdentity s')
-  pure $ runIdentity b
-               )
--- | Run a PDA and keep a list of outputs, states, and stack
-runPDA :: MSF (StateT (Seq stack) Identity) a b -> [a] -> s -> Seq stack -> PDATransition [] b s stack
-runPDA pdaMsf input initialS initialStack = let
-  out = runIdentity $ evalStateT (embed pdaMsf input) initialStack
-  (bs, ss, stacks) = unzip3 out
-  in PDATransition bs ss stacks
+                 (st, sk) <- get
+                 let PDATransition b st' skOp = f a st (viewl sk)
+                 case skOp of
+                   NullOp -> put (st', sk)
+                   Push x -> put (st', x <| sk)
+                   Pop -> put (st', Seq.drop 1 sk)
 
--- | A helper to wrap `Identity`
-pdai :: b -> s -> PDAOp stack -> PDATransition Identity b s stack
-pdai b s op = PDATransition (Identity b) (Identity s) (Identity op)
+                 pure b)
 
--- | Example PDA for recognizing strings of the form a^n b^n
--- (This is a simple example and may not cover all cases)
-examplePDA :: Char -> Q -> Maybe Sym -> PDATransition Identity Bool Q Sym
-examplePDA 'a' Q0 Nothing  = pdai True Q1 (Push A)
-examplePDA 'a' Q1 (Just A) = pdai True Q1 (Push A)
-examplePDA 'b' Q1 (Just A) = pdai True Q2 Pop
-examplePDA 'b' Q2 (Just A) = pdai True Q2 Pop
-examplePDA 'b' Q2 Nothing  = pdai True QAccept NullOp
-examplePDA _ _ _           = pdai False QReject NullOp
+-- | Upgrade an MSF to also output it's state
+outputState' :: MSF (State s) a b -> MSF (State s) a (b, s)
+outputState' msf = proc a -> do
+  b <- msf -< a
+  s <- arrM gets -< id
+  returnA -< (b, s)
 
--- data PDAOp s =
---   NullOp
---   | Push s
---   | Pop
+-- | Run an FSM and keep a list of outputs and states
+runPDA :: MSF (State (PDAState state stack)) a b -> [a] -> state -> ([b], [state], [Seq stack])
+runPDA msf input initialSt = let
+  msf' = proc a -> do
+    b <- msf -< a
+    (st, sk) <- arrM gets -< id
+    returnA -< (b, st, sk)
+  out = evalState (embed msf' input) (initialSt, Seq.empty)
+  in unzip3 out
 
--- -- | Pushdown Automaton (PDA)
--- newtype PDATransition f a b state stack = PDATransition {
---   pdaOutput :: !(f b),
---   pdaState :: !(f b),
---   pdaStack :: !(f b)
---   } deriving (Show, Functor)
-
--- -- | Construct a PDA from a transition function
--- pda :: (a -> state -> Maybe stack -> (b, state, PDAOp stack)) -> PDATransition Identity a b state stack
-
-
-
-
--- pda f = let m = MSF $ (\a -> do
---                           (s, stk) <- get
---                           let (b, s', op) = f a s (viewl stk)
---                           case op of
---                             NullOp -> pure ()
---                             Push x -> put (s', stk |> x)
---                             Pop -> put (s', Seq.drop 1 stk)
---                           pure (b, m))
---   in PDA m
-
--- runPDA :: PDA a b state stack -> [a] -> state -> Seq stack -> ([b], (state, Seq stack))
--- runPDA (PDA msf) input initialState initialStack = runState (go input msf) (initialState, initialStack)
---   where
---     go [] _ = pure []
---     go (x:xs) m = do
---       (b, m') <- unMSF m x
---       bs <- go xs m'
---       pure (b:bs)
-
-
-anbn :: Sym -> Q -> Maybe Sym -> ((), Q, PDAOp Sym)
-anbn A Q0 Nothing = ((), Q1, Push A)
-anbn A Q1 (Just A) = ((), Q1, Push A)
-anbn B Q1 (Just A) = ((), Q2, Pop)
-anbn B Q2 (Just A) = ((), Q2, Pop)
-anbn B Q2 Nothing = ((), QAccept, NullOp)
-anbn _ _ _ = ((), Q0, NullOp)
-
-
--- --------------------------------------------------
--- -- * Turing Machine
-
-
--- data TMOp s =
---   EnqL s
---   | DeqR
---   | TMNullOp
---   deriving (Eq, Show)
-
--- -- | Turing Machine with Queue
--- newtype TM a b state symbol = TM {unTM :: MSF (State (state, Seq symbol)) a b}
-
--- -- | Construct a TM from a transition function
--- tm :: (a -> state -> Maybe symbol -> (b, state, TMOp symbol)) -> TM a b state symbol
--- tm f = let m = MSF $ (\a -> do
---                         (s, q) <- get
---                         let (b, s', op) = f a s (viewl q)
---                         case op of
---                           EnqL new -> put (s', (Seq.<| q) new)
---                           DeqR -> put (s', dropr 1 q)
---                           TMNullOp -> pure ()
---                         pure (b, m))
---   in TM m
-
--- runTM :: TM a b state symbol -> [a] -> state -> Seq symbol -> ([b], (state, Seq symbol))
--- runTM (TM msf) input initialState initialQueue = runState (go input msf) (initialState, initialQueue)
---   where
---     go [] _ = pure []
---     go (x:xs) m = do
---       (b, m') <- unMSF m x
---       bs <- go xs m'
---       pure (b:bs)
-
-
--- anbncn :: TM Sym () Q Sym
--- anbncn = tm transition
---   where
---     transition :: Sym -> Q -> Maybe Sym -> ((), Q, TMOp Sym)
---     transition A Q0 _ = ((), Q0, EnqL A)
---     transition B Q0 (Just A) = ((), Q1, EnqL A)
-
-
---------------------------------------------------
--- Go
+anbn ::
+  Char
+  -> Q    -- state
+  -> Maybe Char -- stack
+  -> PDATransition () Q Char
+-- If the input is '^' and the state is Q0 with an empty stack, push '$' onto the stack and move to Q1
+anbn '^' Q0 Nothing      = PDATransition () Q1 (Push '$')
+-- If the input is '$' and the state is Q1 with '$' on the stack, move to QAccept and pop '$'
+anbn '$' Q1 (Just '$')   = PDATransition () QAccept Pop
+-- If the input is 'a' and the state is Q1 with '$' on the stack, push 'A' onto the stack and stay in Q1
+anbn 'a' Q1 (Just '$')   = PDATransition () Q1 (Push 'A')
+-- If the input is 'a' and the state is Q1 with 'A' on the stack, push 'A' onto the stack and stay in Q1
+anbn 'a' Q1 (Just 'A')   = PDATransition () Q1 (Push 'A')
+-- If the input is 'b' and the state is Q1 with 'A' on the stack, pop 'A' from the stack and move to Q2
+anbn 'b' Q1 (Just 'A')   = PDATransition () Q2 Pop
+-- If the input is 'b' and the state is Q2 with 'A' on the stack, pop 'A' from the stack and stay in Q2
+anbn 'b' Q2 (Just 'A')   = PDATransition () Q2 Pop
+-- If the input is '$' and the state is Q2 with '$' on the stack, move to QAccept and pop '$'
+anbn '$' Q2 (Just '$')   = PDATransition () QAccept Pop
+-- If the input is '$' and the state is Q2 with 'A' on the stack, move to QReject
+anbn '$' Q2 (Just 'A')   = PDATransition () QReject NullOp
+-- For any other input or state configuration, move to QReject
+anbn _ _ _               = PDATransition () QReject NullOp
 
 main :: IO ()
 main = do
@@ -284,38 +246,22 @@ main = do
   let str = "aaabaa"
   print $ runFSM (fsm countA) str 0
 
+  -- PDA
+  putStrLn "----------"
+  putStrLn "-- PDA"
+  let exampleStrings = [
+        "^aaabbb$",
+        "^aabbb$",
+        "^aaabb$",
+        "^ab$",
+        "^$"]
+  putStrLn "Example strings:"
+  mapM_ (putStrLn . formatPDAResult . (\x -> runPDA (pda anbn) x Q0)) exampleStrings
 
-  -- -- PDA
-  -- putStrLn "----------"
-  -- putStrLn "-- PDA"
-  -- let strings = [ [A, A, B, B],
-  --                 [A, A, B, B, B],
-  --                 [A, A, A, B, B]
-  --               ]
-  -- mapM_ (\str -> do
-  --   let (out, (state, stack)) = runPDA anbn str Q0 Seq.empty
-  --   putStrLn $ show str
-  --     ++ " : " ++ show out
-  --     ++ " : " ++ show state
-  --     ++ " : " ++ show stack) strings
+showSeq :: Show a => Seq a -> String
+showSeq xs = "{" ++ intercalate ", " (show <$> toList xs) ++ "}"
 
-
-  -- -- TM
-  -- putStrLn "----------"
-  -- putStrLn "-- Turing Machine"
-  -- let strings = [
-  --       [A],
-  --       [A, B],
-  --       [A, B, C],
-  --       [A, B, C, C],
-  --       [A, B, B, C],
-  --       [A, A, B, B, C, C],
-  --       [A, A, B, B, B, C, C],
-  --       [A, A, A, B, B, C, C, C]
-  --       ]
-  -- mapM_ (\str -> do
-  --   let (out, (state, queue)) = runTM anbncn str Q0 Seq.empty
-  --   putStrLn $ show str
-  --     ++ " : " ++ show out
-  --     ++ " : " ++ show state
-  --     ++ " : " ++ show queue) strings
+formatPDAResult :: ([()], [Q], [Seq Char]) -> String
+formatPDAResult (outs, states, stack) =
+  let finalState = last states
+  in "Final state: " ++ show finalState ++ " | " ++ show (showSeq <$> stack)
