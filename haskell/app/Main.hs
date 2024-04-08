84f00c24c4f03eb67e249245a074e78f0a1999a7
person <freckletonj@gmail.com>
2024-04-08 03:15:46 -0700

    abomination

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -51,6 +51,7 @@ Multi-tape, with k tapes:
 
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
+-- {-# LANGUAGE OverlappingInstances #-}
 
 module Main where
 
@@ -61,6 +62,7 @@ import Data.List (intercalate)
 import qualified Data.Map as M
 import Data.Kind (Type)
 import Data.Foldable (foldl')
+import Data.Map.Extra (lookupMatchAny, Any(..), MatchAny(..))
 
 
 --------------------------------------------------
@@ -96,40 +98,44 @@ class Machine m a (s :: Type) where
   mkInput :: a -> S m a s -> L m a s
 
 
+-- -- | Run a machine on an input symbol
+-- execStep :: (Machine m a s, Ord (L m a s), Show (L m a s))
+--   => M.Map (L m a s) (R m a s) -- transition table
+--   -> a -- single input
+--   -> S m a s -- state
+--   -> S m a s -- new state
+-- execStep table input st =
+--   case M.lookup (mkInput input st) table of
+--     Just transition -> action transition st
+--     Nothing -> error $ "transition not found: " ++ show (mkInput input st)
+
+
+-- -- | Run a machine on a list of input symbols
+-- execMachine :: (Machine m a s, Ord (L m a s), Show (L m a s))
+--   => M.Map (L m a s) (R m a s) -- transition table
+--   -> [a] -- input symbols
+--   -> S m a s -- initial state
+--   -> S m a s
+-- execMachine table input initialState = foldr (execStep table) initialState input
+
+
 -- | Run a machine on an input symbol
-execStep :: (Machine m a s, Ord (L m a s), Show (L m a s))
-  => M.Map (L m a s) (R m a s) -- transition table
-  -> a -- single input
-  -> S m a s -- state
-  -> S m a s -- new state
-execStep table input st =
-  case M.lookup (mkInput input st) table of
-    Just transition -> action transition st
-    Nothing -> error $ "transition not found: " ++ show (mkInput input st)
-
-
--- | Run a machine on a list of input symbols
-execMachine :: (Machine m a s, Ord (L m a s), Show (L m a s))
-  => M.Map (L m a s) (R m a s) -- transition table
-  -> [a] -- input symbols
-  -> S m a s -- initial state
-  -> S m a s
-execMachine table input initialState = foldr (execStep table) initialState input
-
-
--- | Run a machine on an input symbol
-runStep :: (Machine m a s, Ord (L m a s), Show (L m a s))
+runStep :: (Machine m a s, Ord (L m a s), Show (L m a s), MatchAny (L m a s))
   => M.Map (L m a s) (R m a s) -- transition table
   -> S m a s -- state
   -> a -- single input
   -> (R m a s, S m a s) -- (transition value, new state)
 runStep table st input =
-  case M.lookup (mkInput input st) table of
+  case lookupMatchAny (mkInput input st) table of
     Just transition -> (transition, action transition st)
     Nothing -> error $ "transition not found: " ++ show (mkInput input st)
 
 -- | Run a machine on a list of input symbols
-runMachine :: (Machine m a s, Ord (L m a s), Show (L m a s))
+runMachine :: (Machine m a s
+              , Ord (L m a s)
+              , Show (L m a s)
+              , MatchAny (L m a s)
+              )
   => M.Map (L m a s) (R m a s) -- transition table
   -> S m a s -- initial state
   -> [a] -- input symbols
@@ -146,7 +152,7 @@ data PDA s
 data PDAOp stack = NullOp | Push stack | Pop deriving (Eq, Ord, Show)
 
 instance Machine PDA a (state, stack) where
-  data L PDA a (state, stack) = PDAL a state (Maybe stack) deriving (Show, Ord, Eq)
+  data L PDA a (state, stack) = PDAL (Any a) (Any state) (Maybe (Any stack)) deriving (Show, Ord, Eq)
   data R PDA a (state, stack) = PDAR state (PDAOp stack) deriving (Show, Ord, Eq)
   data S PDA a (state, stack) = PDAS state (Data.Sequence.Seq stack) deriving (Show, Ord, Eq)
 
@@ -154,7 +160,12 @@ instance Machine PDA a (state, stack) where
   action (PDAR newState (Push x)) (PDAS _ stack) = PDAS newState (x Data.Sequence.<| stack)
   action (PDAR newState Pop) (PDAS _ stack) = PDAS newState (Seq.drop 1 stack)
 
-  mkInput a (PDAS st sk) = PDAL a st (viewl sk)
+  mkInput a (PDAS st sk) = PDAL (A a) (A st) (A <$> (viewl sk))
+
+
+-- needs OverlappingInstances
+instance (MatchAny a, MatchAny state, Eq stack, Eq a, MatchAny Char) => MatchAny (L PDA a (state, stack)) where
+  matchAny (PDAL x0 x1 x2) (PDAL y0 y1 y2) = matchAny (x0, x1, x2) (y0, y1, y2)
 
 
 ----------
@@ -170,16 +181,28 @@ data Q =
   | QReject
   deriving (Eq, Show, Ord)
 
+
+-- TODO: this is messy
+instance MatchAny Q where
+  matchAny x y = x == y
+
+instance MatchAny Char where
+  matchAny x y = x == y
+
+
 -- | Grammar for: a^nb^n
 anbnTransitions :: [(L PDA Char (Q, Char), R PDA a (Q, Char))]
 anbnTransitions = [
-    (PDAL '^' Q0 Nothing,    PDAR Q1 (Push '$')),
-    (PDAL '$' Q1 (Just '$'), PDAR QAccept Pop),
-    (PDAL 'a' Q1 (Just '$'), PDAR Q1 (Push 'A')),
-    (PDAL 'a' Q1 (Just 'A'), PDAR Q1 (Push 'A')),
-    (PDAL 'b' Q1 (Just 'A'), PDAR Q2 Pop),
-    (PDAL 'b' Q2 (Just 'A'), PDAR Q2 Pop),
-    (PDAL '$' Q2 (Just '$'), PDAR QAccept Pop)
+    (PDAL (A '^') (A Q0) Nothing,    PDAR Q1 (Push '$')),
+    (PDAL (A '$') (A Q1) (Just $ A '$'), PDAR QAccept Pop),
+    (PDAL (A 'a') (A Q1) (Just $ A '$'), PDAR Q1 (Push 'A')),
+    (PDAL (A 'a') (A Q1) (Just $ A 'A'), PDAR Q1 (Push 'A')),
+    (PDAL (A 'b') (A Q1) (Just $ A 'A'), PDAR Q2 Pop),
+    (PDAL (A 'b') (A Q2) (Just $ A 'A'), PDAR Q2 Pop),
+    (PDAL (A '$') (A Q2) (Just $ A '$'), PDAR QAccept Pop),
+
+    (PDAL Any Any Nothing, PDAR QReject NullOp),
+    (PDAL Any Any (Just Any), PDAR QReject NullOp)
   ]
 
 
@@ -189,7 +212,7 @@ anbnTransitions = [
 showSeq :: Show a => Data.Sequence.Seq a -> String
 showSeq xs = "{" ++ intercalate ", " (show <$> toList xs) ++ "}"
 
-formatPDAResult :: (R PDA Char (Q, Char), S PDA Char (Q, Char)) -> String
+--formatPDAResult :: (R PDA Char (Q, Char), S PDA Char (Q, Char)) -> String
 formatPDAResult (_, PDAS finalState stack) =
   "Final state: " ++ show finalState ++ " | Stack: " ++ showSeq stack
 
